9b783a8905e0b49c85f6ceaa4a935b75b2a961bf
Daniel Beltran <genserdanie-123@hotmail.com>
2022-04-06 23:20:58 +0200

    Primera fase del formulario

---
+++ ManejoFormulariosHtml/src/main/webapp/recursos/funciones.js
@@ -0,0 +1,58 @@
+//Contendra el metodo validar forma, que validara cada uno de los elementos del formulario
+
+function validarForma(forma){
+    var usuario = forma.usuario;
+    if(usuario.value == "" || usuario.value == "Escribir Usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+    }
+    
+    var password = forma.password;
+    if(password.value == "" || password.value.length < 3){
+        alert("Debe proporcionar un password al menos de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
+    
+    var tecnologias = forma.tecnologia;
+    var checkSeleccionado = false;
+    
+    for(var i = 0; i < tecnologias.length; i++){
+        if(tecnologias[i].checked){
+            checkSeleccionado = true;
+        }
+    }
+    
+    if(!checkSeleccionado){
+        alert("Debe selecionar una tecnologia");
+        return false;
+    }
+    
+    var genero = forma.genero;
+    var radioSeleccionado = false;
+    
+    for(var i = 0; i < genero.length; i++){
+        if(genero[i].checked){
+            radioSeleccionado = true;
+        }
+    }
+    if(!radioSeleccionado){
+        alert("Debe selecionar un genero");
+        return false;
+    }
+    
+    var ocupacion = forma.ocupacion;
+    if(ocupacion.value == ""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    
+    //Formulario es valido
+    alert("Formulario es valido, enviado al servidor");
+    return true;
+    
+}
+
